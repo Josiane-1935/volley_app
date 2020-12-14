397c4c257f79504c0cd9da5bf19d4cdfc3793022
MangoJosiane <mango.heri@strathmore.edu>
2020-12-14 11:57:43 +0300

    volley_app

---
+++ app/src/main/java/com/example/volley_app/DevelopersList.java
@@ -0,0 +1,28 @@
+package com.example.volley_app;
+
+
+
+
+public class DevelopersList {
+    private String login;
+    private String avatar_url;
+    private String html_url;
+
+    public DevelopersList(String login, String avatar_url, String html_url) {
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.html_url = html_url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+}
